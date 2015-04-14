b63644c257d234fe3c5ec5932c2832cd74a95dcb
Draveness <stark.draven@gmail.com>
2015-04-14 22:27:35 +0800

    Initial Commit

---
+++ DKNightVersion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DKNightVersion
+//
+//  Created by apple on 4/14/15.
+//  Copyright (c) 2015 DeltaX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DKNightVersion/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DKNightVersion
+//
+//  Created by apple on 4/14/15.
+//  Copyright (c) 2015 DeltaX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
