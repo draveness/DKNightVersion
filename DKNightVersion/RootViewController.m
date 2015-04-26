fe1e7afc9adb0731540f68426a35fa3273904059
Draveness <stark.draven@gmail.com>
2015-04-26 16:32:16 +0800

    Update demo button color

--- DKNightVersion/RootViewController.m
+++ DKNightVersion/RootViewController.m
@@ -31,13 +31,13 @@
     UIButton *nightButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 200, 50)];
     nightButton.center = CGPointMake(self.view.center.x, 450);
     [nightButton setTitle:@"Night Falls" forState:UIControlStateNormal];
-    [nightButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [nightButton setTitleColor:[UIColor colorWithRed:0.478 green:0.651 blue:0.988 alpha:1.0] forState:UIControlStateNormal];
     [nightButton addTarget:self action:@selector(nightFalls) forControlEvents:UIControlEventTouchUpInside];
 
     UIButton *dawnButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 200, 50)];
     [dawnButton setTitle:@"Dawn Comes" forState:UIControlStateNormal];
     dawnButton.center = CGPointMake(self.view.center.x, 500);
-    [dawnButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [dawnButton setTitleColor:[UIColor colorWithRed:0.478 green:0.651 blue:0.988 alpha:1.0] forState:UIControlStateNormal];
     [dawnButton addTarget:self action:@selector(dawnComes) forControlEvents:UIControlEventTouchUpInside];
 
     self.view.nightBackgroundColor = [UIColor colorWithRed:0.141 green:0.145 blue:0.153 alpha:1.0];
