6c967709a61c8faf9068c768fc781d54519765a9
Draveness <stark.draven@gmail.com>
2015-04-26 16:57:13 +0800

    Add gif

--- DKNightVersion/RootViewController.m
+++ DKNightVersion/RootViewController.m
@@ -18,9 +18,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 200, 100)];
-    label.center = self.view.center;
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 300, 100)];
+    label.center = CGPointMake(self.view.center.x, 240);
     label.clipsToBounds = YES;
+    label.font = [UIFont systemFontOfSize:25];
     label.layer.cornerRadius = 10;
     label.numberOfLines = 0;
     label.text = @"DKNightVersion is a light weight framework adding night version to your app. ";
@@ -29,14 +30,16 @@
     label.nightTextColor = [UIColor colorWithRed:0.396 green:0.400 blue:0.408 alpha:1.0];
 
     UIButton *nightButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 200, 50)];
-    nightButton.center = CGPointMake(self.view.center.x, 450);
+    nightButton.center = CGPointMake(self.view.center.x, 350);
+    nightButton.titleLabel.font = [UIFont systemFontOfSize:30];
     [nightButton setTitle:@"Night Falls" forState:UIControlStateNormal];
     [nightButton setTitleColor:[UIColor colorWithRed:0.478 green:0.651 blue:0.988 alpha:1.0] forState:UIControlStateNormal];
     [nightButton addTarget:self action:@selector(nightFalls) forControlEvents:UIControlEventTouchUpInside];
 
-    UIButton *dawnButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 200, 50)];
+    UIButton *dawnButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 300, 50)];
     [dawnButton setTitle:@"Dawn Comes" forState:UIControlStateNormal];
-    dawnButton.center = CGPointMake(self.view.center.x, 500);
+    dawnButton.titleLabel.font = [UIFont systemFontOfSize:30];
+    dawnButton.center = CGPointMake(self.view.center.x, 450);
     [dawnButton setTitleColor:[UIColor colorWithRed:0.478 green:0.651 blue:0.988 alpha:1.0] forState:UIControlStateNormal];
     [dawnButton addTarget:self action:@selector(dawnComes) forControlEvents:UIControlEventTouchUpInside];
 
