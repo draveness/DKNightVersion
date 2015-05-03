6b34f7650f9ca292f97297bc3ce6ab026c56e3fc
Draveness <stark.draven@gmail.com>
2015-05-03 16:05:22 +0800

    Remove useless file

--- try.rb
+++
@@ -1,8 +0,0 @@
-require 'xcodeproj'
-
-proj = Xcodeproj::Project.new('DKNightVersion.xcodeproj')
-implm_ref = proj.main_group.new_file('./guess.m')
-app_target = proj.targets.first
-app_target.add_file_references([implm_ref])
-
-proj.save
