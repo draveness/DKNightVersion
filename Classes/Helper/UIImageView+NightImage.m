ed4556c79b0d609fb623791b5f5471c8693acdcd
Draveness <stark.draven@gmail.com>
2015-07-11 09:03:51 +0800

    Use method swizzling to change set image method imp

--- Classes/Helper/UIImageView+NightImage.m
+++ Classes/Helper/UIImageView+NightImage.m
@@ -7,6 +7,7 @@
 //
 
 #import "UIImageView+NightImage.h"
+#import "DKNightVersionManager.h"
 #import <objc/runtime.h>
 
 @interface UIView ()
@@ -17,6 +18,31 @@
 
 
 @implementation UIImageView (NightImage)
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Class class = [self class];
+        SEL originalSelector = @selector(setImage:);
+        SEL swizzledSelector = @selector(hook_setImage:);
+        Method originalMethod = class_getInstanceMethod(class, originalSelector);
+        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+        BOOL didAddMethod =
+        class_addMethod(class, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
+        if (didAddMethod){
+            class_replaceMethod(class, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
+        } else {
+            method_exchangeImplementations(originalMethod, swizzledMethod);
+        }
+    });
+}
+
+- (void)hook_setImage:(UIImage *)image {
+    if ([DKNightVersionManager currentThemeVersion] == DKThemeVersionNormal) {
+
+    }
+    [self hook_setImage:image];
+}
 
 - (UIImage *)nightImage {
     return objc_getAssociatedObject(self, @selector(nightImage));
