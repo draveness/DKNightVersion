3df3aa54336136a1be8cc824243faae392848bfe
Draveness <stark.draven@gmail.com>
2015-11-11 15:44:41 +0800

    Updated animation duration

--- Classes/Core/DKNightVersionManager.m
+++ Classes/Core/DKNightVersionManager.m
@@ -11,7 +11,7 @@
 NSString *const DKNightVersionNightFallingNotification = @"DKNightVersionNightFallingNotification";
 NSString *const DKNightVersionDawnComingNotification = @"DKNightVersionDawnComingNotification";
 
-CGFloat const DKNightVersionAnimationDuration = 0.3f;
+CGFloat const DKNightVersionAnimationDuration = 0;
 
 @interface DKNightVersionManager ()
 
