b28e0ff3c311537e20f79f88d31c8719c94d6e37
Draveness <stark.draven@gmail.com>
2015-11-21 13:14:33 +0800

    Reindent weakify and strongifty

--- Classes/DKNightVersion.h
+++ Classes/DKNightVersion.h
@@ -22,22 +22,22 @@
 
 #endif
 
-#ifndef    weakify
+#ifndef weakify
 #if __has_feature(objc_arc)
 
 #define weakify( x ) \
-_Pragma("clang diagnostic push") \
-_Pragma("clang diagnostic ignored \"-Wshadow\"") \
-autoreleasepool{} __weak __typeof__(x) __weak_##x##__ = x; \
-_Pragma("clang diagnostic pop")
+    _Pragma("clang diagnostic push") \
+    _Pragma("clang diagnostic ignored \"-Wshadow\"") \
+    autoreleasepool{} __weak __typeof__(x) __weak_##x##__ = x; \
+    _Pragma("clang diagnostic pop")
 
 #else
 
 #define weakify( x ) \
-_Pragma("clang diagnostic push") \
-_Pragma("clang diagnostic ignored \"-Wshadow\"") \
-autoreleasepool{} __block __typeof__(x) __block_##x##__ = x; \
-_Pragma("clang diagnostic pop")
+    _Pragma("clang diagnostic push") \
+    _Pragma("clang diagnostic ignored \"-Wshadow\"") \
+    autoreleasepool{} __block __typeof__(x) __block_##x##__ = x; \
+    _Pragma("clang diagnostic pop")
 
 #endif
 #endif
