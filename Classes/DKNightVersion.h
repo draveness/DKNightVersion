6949d173c14a465c67edc5fb9396e5b0c505d8bb
Draveness <stark.draven@gmail.com>
2015-12-09 23:28:20 +0800

    Removed 'weakify' and 'strongify'

--- Classes/DKNightVersion.h
+++ Classes/DKNightVersion.h
@@ -30,45 +30,4 @@
 #import "UISlider+Night.h"
 #import "UISwitch+Night.h"
 
-
-#endif
-
-#ifndef weakify
-#if __has_feature(objc_arc)
-
-#define weakify( x ) \
-    _Pragma("clang diagnostic push") \
-    _Pragma("clang diagnostic ignored \"-Wshadow\"") \
-    autoreleasepool{} __weak __typeof__(x) __weak_##x##__ = x; \
-    _Pragma("clang diagnostic pop")
-
-#else
-
-#define weakify( x ) \
-    _Pragma("clang diagnostic push") \
-    _Pragma("clang diagnostic ignored \"-Wshadow\"") \
-    autoreleasepool{} __block __typeof__(x) __block_##x##__ = x; \
-    _Pragma("clang diagnostic pop")
-
-#endif
-#endif
-
-#ifndef    strongify
-#if __has_feature(objc_arc)
-
-#define strongify( x ) \
-_Pragma("clang diagnostic push") \
-_Pragma("clang diagnostic ignored \"-Wshadow\"") \
-try{} @finally{} __typeof__(x) x = __weak_##x##__; \
-_Pragma("clang diagnostic pop")
-
-#else
-
-#define strongify( x ) \
-_Pragma("clang diagnostic push") \
-_Pragma("clang diagnostic ignored \"-Wshadow\"") \
-try{} @finally{} __typeof__(x) x = __block_##x##__; \
-_Pragma("clang diagnostic pop")
-
-#endif
 #endif
