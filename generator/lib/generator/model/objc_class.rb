3a519b6f26068de6ab1d8569ec2b5d3efa767bf0
Draveness <stark.draven@gmail.com>
2015-12-09 22:25:41 +0800

    Removed unnecessary codes in 'objc_class'

--- generator/lib/generator/model/objc_class.rb
+++ generator/lib/generator/model/objc_class.rb
@@ -14,22 +14,6 @@ class ObjcClass
         @properties = properties
 	end
 
-	def nightversion_header_name
-		name + '+NightVersion.h'
-	end
-
-	def nightversion_imp_name
-		name + '+NightVersion.m'
-	end
-
-	def color_header_name(property)
-		file_name(property) + '.h'
-	end
-	
-	def color_imp_name(property)
-		file_name(property) + '.m'
-	end
-
     def header_name
         "#{name}+Night.h"
     end
@@ -73,9 +57,5 @@ class ObjcClass
     end
 
 private
-
-	def file_name(property)
-		"#{name}+#{property.cap_name}"
-	end
 
 end
