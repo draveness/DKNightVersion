b1a206053fa62fdbaf0cc51f52ed550cf00c5cdc
Draveness <stark.draven@gmail.com>
2015-12-09 18:05:05 +0800

    Complete 'DKColor' basic infrastructure

--- Classes/Core/DKColor.h
+++ Classes/Core/DKColor.h
@@ -11,4 +11,32 @@
 
 @interface DKColor : NSObject
 
+- (DKColorPicker)pickerWithUIColor:(UIColor *)color;
+
++ (DKColorPicker)pickerWithUIColor:(UIColor *)color;
+
+- (DKColorPicker)blackColor;
+
+- (DKColorPicker)lightGrayColor;
+
+- (DKColorPicker)grayColor;
+
+- (DKColorPicker)greenColor;
+
+- (DKColorPicker)cyanColor;
+
+- (DKColorPicker)magentaColor;
+
+- (DKColorPicker)purpleColor;
+
+- (DKColorPicker)clearColor;
+
++ (DKColorPicker)colorWithHue:(CGFloat)hue saturation:(CGFloat)saturation brightness:(CGFloat)brightness alpha:(CGFloat)alpha;
+
++ (DKColorPicker)colorWithCGColor:(CGColorRef)cgColor;
+
+#if __has_include(<CoreImage/CoreImage.h>)
++ (DKColorPicker)colorWithCIColor:(CIColor *)ciColor NS_AVAILABLE_IOS(5_0);
+#endif
+
 @end
--- Classes/Core/DKColor.m
+++ Classes/Core/DKColor.m
@@ -10,4 +10,103 @@
 
 @implementation DKColor
 
+- (DKColorPicker)pickerWithUIColor:(UIColor *)color {
+    return ^() {
+        return color;
+    };
+}
+
++ (DKColorPicker)pickerWithUIColor:(UIColor *)color {
+    return ^() {
+        return color;
+    };
+}
+
+- (DKColorPicker)blackColor {
+    return [self pickerWithUIColor:[UIColor blackColor]];
+}
+
+- (DKColorPicker)darkGrayColor {
+    return [self pickerWithUIColor:[UIColor darkGrayColor]];
+}
+
+- (DKColorPicker)lightGrayColor {
+    return [self pickerWithUIColor:[UIColor lightGrayColor]];
+}
+
+- (DKColorPicker)whiteColor {
+    return [self pickerWithUIColor:[UIColor whiteColor]];
+}
+
+- (DKColorPicker)grayColor {
+    return [self pickerWithUIColor:[UIColor grayColor]];
+}
+
+- (DKColorPicker)redColor {
+    return [self pickerWithUIColor:[UIColor redColor]];
+}
+
+- (DKColorPicker)greenColor {
+    return [self pickerWithUIColor:[UIColor greenColor]];
+}
+
+- (DKColorPicker)blueColor {
+    return [self pickerWithUIColor:[UIColor blueColor]];
+}
+
+- (DKColorPicker)cyanColor {
+    return [self pickerWithUIColor:[UIColor cyanColor]];
+}
+
+- (DKColorPicker)yellowColor {
+    return [self pickerWithUIColor:[UIColor yellowColor]];
+}
+
+- (DKColorPicker)magentaColor {
+    return [self pickerWithUIColor:[UIColor magentaColor]];
+}
+
+- (DKColorPicker)orangeColor {
+    return [self pickerWithUIColor:[UIColor orangeColor]];
+}
+
+- (DKColorPicker)purpleColor {
+    return [self pickerWithUIColor:[UIColor purpleColor]];
+}
+
+- (DKColorPicker)brownColor {
+    return [self pickerWithUIColor:[UIColor brownColor]];
+}
+
+- (DKColorPicker)clearColor {
+    return [self pickerWithUIColor:[UIColor clearColor]];
+}
+
++ (DKColorPicker)colorWithWhite:(CGFloat)white alpha:(CGFloat)alpha {
+    return [self pickerWithUIColor:[UIColor colorWithWhite:white alpha:alpha]];
+}
+
++ (DKColorPicker)colorWithHue:(CGFloat)hue saturation:(CGFloat)saturation brightness:(CGFloat)brightness alpha:(CGFloat)alpha {
+    return [self pickerWithUIColor:[UIColor colorWithHue:hue saturation:saturation brightness:brightness alpha:alpha]];
+}
+
++ (DKColorPicker)colorWithRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue alpha:(CGFloat)alpha {
+    return [self pickerWithUIColor:[UIColor colorWithRed:red green:green blue:blue alpha:alpha]];
+}
+
++ (DKColorPicker)colorWithCGColor:(CGColorRef)cgColor {
+    return [self pickerWithUIColor:[UIColor colorWithCGColor:cgColor]];
+}
+
++ (DKColorPicker)colorWithPatternImage:(UIImage *)image {
+    return [self pickerWithUIColor:[UIColor colorWithPatternImage:image]];
+}
+
+#if __has_include(<CoreImage/CoreImage.h>)
++ (DKColorPicker)colorWithCIColor:(CIColor *)ciColor NS_AVAILABLE_IOS(5_0) {
+    return [self pickerWithUIColor:[UIColor colorWithCIColor:ciColor]];
+}
+#endif
+
 @end
+
