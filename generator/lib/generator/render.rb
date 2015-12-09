002356db6e40848ef2318cffa1fccfdb8476f530
Draveness <stark.draven@gmail.com>
2015-12-09 17:42:34 +0800

    refine ruby script

--- generator/lib/generator/render.rb
+++ generator/lib/generator/render.rb
@@ -31,8 +31,6 @@ def objc_code_generator(klasses, p='.')
 	template_folder = File.join('generator', 'lib', 'generator', 'template')
 	color_header        = File.join(template_folder, 'color.h.erb')
 	color_imp           = File.join(template_folder, 'color.m.erb')
-	#nightversion_header = File.join(template_folder, 'nightversion.h.erb')
-	#nightversion_imp    = File.join(template_folder, 'nightversion.m.erb')
 
 	relative_path = File.join('Classes', 'UIKit')
 	FileUtils.rm_rf(relative_path)
@@ -43,19 +41,8 @@ def objc_code_generator(klasses, p='.')
 
         path = File.join(p, 'Classes', 'UIKit', klass.name)
         groups[klass.name] = []
-        #groups[klass.name] << File.join(path, klass.nightversion_header_name)
-        #groups[klass.name] << File.join(path, klass.nightversion_imp_name)
-
-        #nightversion_header_file_path = File.join(subfolder_path, klass.nightversion_header_name)
-        #nightversion_imp_file_path    = File.join(subfolder_path, klass.nightversion_imp_name)
-        #puts "[Generate] Generating #{nightversion_header_file_path}"
-		#File.write nightversion_header_file_path, render(nightversion_header, klass)
-        #puts "[Generate] Generating #{nightversion_imp_file_path}"
-		#File.write nightversion_imp_file_path,    render(nightversion_imp,    klass)
 
 		klass.properties.each do |property|
-			#superklass_has_property = has_property(klass.superklass, property.name) if klass.superklass
-
             groups[klass.name] << File.join(path, klass.color_header_name(property))
             groups[klass.name] << File.join(path, klass.color_imp_name(property))
 
