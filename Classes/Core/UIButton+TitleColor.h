7d2dcd2ad8f2d764c695813dafca0d63d8e93fbe
Draveness <stark.draven@gmail.com>
2015-12-09 19:00:49 +0800

    Added 'UIButton+TitleColor' files

---
+++ Classes/Core/UIButton+TitleColor.h
@@ -0,0 +1,13 @@
+//
+//  UIButton+TitleColor.h
+//  DKNightVersion
+//
+//  Created by Draveness on 15/12/9.
+//  Copyright © 2015年 DeltaX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (TitleColor)
+
+@end
---
+++ Classes/Core/UIButton+TitleColor.m
@@ -0,0 +1,13 @@
+//
+//  UIButton+TitleColor.m
+//  DKNightVersion
+//
+//  Created by Draveness on 15/12/9.
+//  Copyright © 2015年 DeltaX. All rights reserved.
+//
+
+#import "UIButton+TitleColor.h"
+
+@implementation UIButton (TitleColor)
+
+@end
