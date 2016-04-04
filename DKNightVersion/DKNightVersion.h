6f49fe976a26bbf302f4a9c1ee82f11e0652417c
Draveness <stark.draven@gmail.com>
2016-04-04 14:02:33 +0800

    Use " instead of angle bracket

--- DKNightVersion/DKNightVersion.h
+++ DKNightVersion/DKNightVersion.h
@@ -14,15 +14,15 @@ FOUNDATION_EXPORT double DKNightVersionVersionNumber;
 #ifndef _DKNIGHTVERSION_
 #define _DKNIGHTVERSION_
 
-#import <Core/DKColor.h>
-#import <Core/DKImage.h>
-#import <Core/DKNightVersionManager.h>
-#import <Core/NSObject+Night.h>
+#import "Core/DKColor.h"
+#import "Core/DKImage.h"
+#import "Core/DKNightVersionManager.h"
+#import "Core/NSObject+Night.h"
 
-#import <ColorTable/DKColorTable.h>
+#import "ColorTable/DKColorTable.h"
 
-#import <UIKit/UIKit+Night.h>
-#import <CoreAnimation/CoreAnimation+Night.h>
+#import "UIKit/UIKit+Night.h"
+#import "CoreAnimation/CoreAnimation+Night.h"
 
 
 #endif /* _DKNIGHTVERSION_ */
