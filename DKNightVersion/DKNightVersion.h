2e26486835bc8abe61131865e9dc303ab481ecca
Draveness <stark.draven@gmail.com>
2016-04-04 16:42:59 +0800

    Remove UIKit category

--- DKNightVersion/DKNightVersion.h
+++ DKNightVersion/DKNightVersion.h
@@ -21,8 +21,25 @@ FOUNDATION_EXPORT double DKNightVersionVersionNumber;
 
 #import "DKColorTable.h"
 
-#import "UIKit+Night.h"
 #import "CoreAnimation+Night.h"
+
+#import "UIBarButtonItem+Night.h"
+#import "UIControl+Night.h"
+#import "UILabel+Night.h"
+#import "UINavigationBar+Night.h"
+#import "UIPageControl+Night.h"
+#import "UIProgressView+Night.h"
+#import "UISearchBar+Night.h"
+#import "UISlider+Night.h"
+#import "UISwitch+Night.h"
+#import "UITabBar+Night.h"
+#import "UITableView+Night.h"
+#import "UITextField+Night.h"
+#import "UITextView+Night.h"
+#import "UIToolbar+Night.h"
+#import "UIView+Night.h"
+#import "UIButton+Night.h"
+#import "UIImageView+Night.h"
 
 
 #endif /* _DKNIGHTVERSION_ */
