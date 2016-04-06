07b8422ff8e95720d3ab6e5de691d190335c2e51
Draveness <stark.draven@gmail.com>
2016-04-06 12:17:33 +0800

    Add comments

--- DKNightVersion/Core/DKImage.h
+++ DKNightVersion/Core/DKImage.h
@@ -12,8 +12,27 @@ typedef NSString DKThemeVersion;
 
 typedef UIImage *(^DKImagePicker)(DKThemeVersion *themeVersion);
 
+/**
+ *  A C function takes an array of images return a image picker, the
+ *  order of the images is just like the themes order in DKColorTable.txt
+ *  file.
+ *
+ *  @param images An array of images
+ *
+ *  @return A DKImagePicker
+ */
 DKImagePicker DKImagePickerWithImages(NSArray<UIImage *> *images);
-DKImagePicker DKImagePickerWithNames(NSArray<NSString *> *images);
+
+/**
+ *  A C function takes an array of names return a image picker, the
+ *  order of the images is just like the themes order in DKColorTable.txt
+ *  file.
+ *
+ *  @param names An array of image names
+ *
+ *  @return A DKImagePicker
+ */
+DKImagePicker DKImagePickerWithNames(NSArray<NSString *> *names);
 
 @interface DKImage : NSObject
 
