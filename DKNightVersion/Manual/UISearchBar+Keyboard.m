a9edf750bdab8524b5341b61140d9bfca0a344a4
Draveness <stark.draven@gmail.com>
2016-06-08 13:21:51 +0800

    Support search bar changing keyboardAppearance feature

--- DKNightVersion/Manual/UISearchBar+Keyboard.m
+++ DKNightVersion/Manual/UISearchBar+Keyboard.m
@@ -7,7 +7,71 @@
 //
 
 #import "UISearchBar+Keyboard.h"
+#import "NSObject+Night.h"
+#import <objc/runtime.h>
+
+@interface NSObject ()
+
+- (void)night_updateColor;
+
+@end
 
 @implementation UISearchBar (Keyboard)
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Class class = [self class];
+
+        SEL originalSelector = @selector(init);
+        SEL swizzledSelector = @selector(dk_init);
+
+        Method originalMethod = class_getInstanceMethod(class, originalSelector);
+        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+
+        BOOL didAddMethod =
+        class_addMethod(class,
+                        originalSelector,
+                        method_getImplementation(swizzledMethod),
+                        method_getTypeEncoding(swizzledMethod));
+
+        if (didAddMethod) {
+            class_replaceMethod(class,
+                                swizzledSelector,
+                                method_getImplementation(originalMethod),
+                                method_getTypeEncoding(originalMethod));
+        } else {
+            method_exchangeImplementations(originalMethod, swizzledMethod);
+        }
+    });
+
+}
+
+- (instancetype)dk_init {
+    UITextView *obj = [self dk_init];
+    if (self.dk_manager.supportsKeyboard && [self.dk_manager.themeVersion isEqualToString:DKThemeVersionNight]) {
+#ifdef __IPHONE_7_0
+        obj.keyboardAppearance = UIKeyboardAppearanceDark;
+#else
+        obj.keyboardAppearance = UIKeyboardAppearanceAlert;
+#endif
+    } else {
+        obj.keyboardAppearance = UIKeyboardAppearanceDefault;
+    }
+    return obj;
+}
+
+- (void)night_updateColor {
+    [super night_updateColor];
+    if (self.dk_manager.supportsKeyboard && [self.dk_manager.themeVersion isEqualToString:DKThemeVersionNight]) {
+#ifdef __IPHONE_7_0
+        self.keyboardAppearance = UIKeyboardAppearanceDark;
+#else
+        self.keyboardAppearance = UIKeyboardAppearanceAlert;
+#endif
+    } else {
+        self.keyboardAppearance = UIKeyboardAppearanceDefault;
+    }
+}
 
 @end
