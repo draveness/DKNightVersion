dfeb9d1dd49f22c287f77bda164a31a5cb7f91f8
taoye <yetao@cienet.com.cn>
2016-11-16 22:13:36 +0800

    add support to CAShapeLayer

---
+++ DKNightVersion/CoreAnimation/CAShapeLayer+Night.h
@@ -0,0 +1,17 @@
+//
+//  CAShapeLayer+Night.h
+//  tztMobileApp_HTSC
+//
+//  Created by YeTao on 2016/11/15.
+//
+//
+
+#import <QuartzCore/QuartzCore.h>
+#import "NSObject+Night.h"
+
+@interface CAShapeLayer (Night)
+
+@property (nonatomic, copy) DKColorPicker dk_strokeColorPicker;
+@property (nonatomic, copy) DKColorPicker dk_fillColorPicker;
+
+@end
